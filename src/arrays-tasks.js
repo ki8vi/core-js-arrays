7c596dc6d1dd3781b8f0250d2b83ff2f7c50a307
ki8vi <oneofbln@gmail.com>
2024-01-15 21:55:49 +0600

    feat: add solut findAllOccurrences task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -82,8 +82,15 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let occurenceNum = 0;
+  arr.map((el) => {
+    if (el === item) {
+      occurenceNum += 1;
+    }
+    return occurenceNum;
+  });
+  return occurenceNum;
 }
 
 /**
