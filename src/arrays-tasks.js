d8f31771482692c6faf70a2d09aa24b78f284d60
ki8vi <oneofbln@gmail.com>
2024-01-21 13:10:54 +0600

    feat: add solution for sortDigitNamesByNumericOrder task (core-js-101 st0)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -610,8 +610,35 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numUnits = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    zero: 0,
+  };
+  const newArr = [];
+  const output = [];
+  const valuesArr = Object.values(numUnits);
+  const keysArr = Object.keys(numUnits);
+  arr.map((el) => {
+    if (numUnits[el] >= 0) {
+      newArr.push(numUnits[el]);
+      newArr.sort((a, b) => a - b);
+    }
+    return newArr;
+  });
+  newArr.map((el) => {
+    output.push(keysArr[valuesArr.indexOf(el)]);
+    return output;
+  });
+  return output;
 }
 
 /**
